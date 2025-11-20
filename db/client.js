70ef27d98cc7635ad8566e70977d2f1d2b2b04f1
Angel Polanco <angelepolanco325@gmail.com>
2025-11-19 23:47:07 -0500

    client

--- db/client.js
+++ db/client.js
@@ -1,3 +1,19 @@
 import pg from "pg";
-const db = new pg.Client(process.env.DATABASE_URL);
-export default db;
+import dotenv from "dotenv";
+
+dotenv.config();
+
+const { Pool } = pg;
+
+export const pool = new Pool({
+  connectionString:
+    process.env.DATABASE_URL || "postgresql://localhost/fullstack_employees",
+  ssl:
+    process.env.NODE_ENV === "production"
+      ? { rejectUnauthorized: false }
+      : false,
+});
+
+export default {
+  query: (text, params) => pool.query(text, params),
+};
